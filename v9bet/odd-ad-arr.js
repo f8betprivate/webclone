6180bf8e4a3a63801634af10d76ae02c06d307a2
Google <opensource@google.com>
2024-03-18 15:48:20 +0700

    new web

---
+++ v9bet/odd-ad-arr.js
@@ -0,0 +1,8 @@
+var ads_arr = [
+ {
+   image: 'https://188.166.214.251/wp-content/uploads/2024/03/v9bet.gif',
+   title: 'v9bet',
+   link: 'https://188.166.214.251/go/v9bet',
+   type: 'image' // fixed
+  }
+];
